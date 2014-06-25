42521ab64a18a53a8cfee2f561ba4d4e0f165635
Tyrone Rogers <tyronerogersjr@gmail.com>
2014-06-25 11:43:29 -0400

    Created a project

---
+++ Tclass/Tclass/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tclass.Startup))]
+namespace Tclass
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
